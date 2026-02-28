1b9205a53fc0c8ede6cd56c6a6d2f6e4c978f374
kareem <sabryk521@gmail.com>
2026-02-28 22:02:13 +0200

    Create pa.py

---
+++ pa.py
@@ -0,0 +1,16 @@
+
+#num1=input("Enter first number: ")
+#num2=input("Enter second number: ")
+#result=float(num1)+float(num2)
+#print(result)
+
+names = [1,"string","python"]
+print(names)
+print(names[0])
+print(names[2])
+print(names[-2]) #start from end
+print(names[0:2])
+print(names[1:])
+print(names[:2])
+names[0]= "one"
+print(names[0])
